a16aa0166e496eaf2dd24b9178a510dfbd4187bf
Noah Stride <noah.stride@goteleport.com>
2022-10-19 18:34:49 +0100

    Basic `teleport-actions/setup` command (#2)

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# teleport-actions
-Mono-repository of GitHub Actions for Teleport
+<div align="center">
+   <img src="./assets/img/readme-header.png" width=750/>
+   <div align="center" style="padding: 25px">
+      <a href="https://www.apache.org/licenses/LICENSE-2.0">
+      <img src="https://img.shields.io/badge/Apache-2.0-red.svg" />
+      </a>
+   </div>
+</div>
+</br>
+
+> Read our Blog: <https://goteleport.com/blog/>
+
+> Read our Documentation: <https://goteleport.com/docs/getting-started/>
+
+## Table of Contents
+
+1. [Introduction](#introduction)
+1. [Actions](#actions)
+1. [Developing](#developing)
+
+## Introduction
+
+`teleport-actions` is a collection of handy GitHub Actions to use within your
+workflows when trying to interact with resources protected by Teleport 🚀.
+
+This project is currently experimental and more information about
+`teleport-actions` will arrive soon.
+
+## Actions
+
+### `@gravitational/teleport-actions/setup`
+
+`setup` installs key Teleport binaries into your workflow environment, for
+example `tctl`, `tsh` and `tbot`. You can then use these within your workflows.
+
+The GitHub Actions tool cache is used by the `setup` action in order to increase
+setup speed and reduce bandwidth usage on self-hosted runners.
+
+Usage:
+
+```yaml
+steps:
+- name: Checkout repository
+   uses: actions/checkout@v3
+- name: Setup teleport
+   uses: gravitational/teleport-actions/setup@v1
+   with:
+      # version must be specified, and exclude the "v" prefix.
+      # check https://goteleport.com/download/ for valid releases.
+      version: 10.3.1
+```
+
+## Developing
+
+This guidance applies to engineers wishing to make changes to Teleport's
+GitHub actions.
+
+Until we build a CI/CD flow for producing the built actions, we have provided
+a series of pre-commit hooks. These will ensure source files follow linter rules
+and also ensure that the built version of the action is up to date. Follow the
+quick-start instructions at <https://pre-commit.com/#quick-start> to install
+the pre-commit hooks.
+
+See the GitHub guidance for versioning:
+<https://github.com/actions/toolkit/blob/main/docs/action-versioning.md#recommendations>
+
+In order to allow us to make fixes to older major versions, we maintain a
+`branch/v${version}` which will diverge from `main`, with changes backported
+from `main` to the appropriate version branch.
---
+++ setup/index.ts
@@ -0,0 +1,98 @@
+import os from 'os';
+
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+
+function getPlatform(rawPlatform: string): string {
+  switch (rawPlatform) {
+    case 'linux': {
+      return 'linux';
+    }
+  }
+  throw new Error(`platform ${rawPlatform} not supported`);
+}
+
+function getArch(rawArch: string): string {
+  switch (rawArch) {
+    case 'x64': {
+      return 'amd64';
+    }
+    case 'arm': {
+      return 'arm';
+    }
+    case 'arm64': {
+      return 'arm64';
+    }
+  }
+  throw new Error(`architecture ${rawArch} not supported`);
+}
+
+/**
+ * versionString converts a requested version, OS and architecture to a format
+ * which can be used to fetch a bundle from the Teleport download site.
+ */
+function versionString(
+  rawPlatform: string,
+  rawArch: string,
+  version: string
+): string {
+  const platform = getPlatform(rawPlatform);
+  const arch = getArch(rawArch);
+
+  return `v${version}-${platform}-${arch}`;
+}
+
+interface Input {
+  version: string;
+}
+
+function getInput(): Input {
+  const version = core.getInput('version');
+  if (version === '') {
+    throw new Error("'version' input must be non-empty");
+  }
+  if (version.startsWith('v')) {
+    throw new Error("'version' input should not be prefixed with 'v'");
+  }
+
+  return {
+    version,
+  };
+}
+
+const toolName = 'teleport';
+
+async function run(): Promise<void> {
+  try {
+    const input = getInput();
+    const version = versionString(os.platform(), os.arch(), input.version);
+    core.info(`Installing Teleport ${version}`);
+
+    const toolPath = tc.find(toolName, version);
+    if (toolPath !== '') {
+      core.info('Teleport binaries found in cache.');
+      core.addPath(toolPath);
+      return;
+    }
+
+    core.info('Could not find Teleport binaries in cache. Fetching...');
+    core.debug('Downloading tar');
+    const downloadPath = await tc.downloadTool(
+      `https://get.gravitational.com/teleport-${version}-bin.tar.gz`
+    );
+
+    core.debug('Extracting tar');
+    const extractedPath = await tc.extractTar(downloadPath, undefined, [
+      'xz',
+      '--strip',
+      '1',
+    ]);
+
+    core.info('Fetched binaries from Teleport. Writing them back to cache...');
+    const cachedPath = await tc.cacheDir(extractedPath, toolName, version);
+    core.addPath(cachedPath);
+  } catch (err: any) {
+    core.setFailed(err.message || err);
+  }
+}
+run();
